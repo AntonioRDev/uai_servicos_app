3bdf8c5e57def8b680fb900f8bc363947b40ceab
Lucas Braga Ferreira <42786572+lucasbrafer@users.noreply.github.com>
2020-12-03 21:20:08 -0300

    new - see more page

--- src/screens/SeeMore/index.jsx
+++ src/screens/SeeMore/index.jsx
@@ -1,8 +1,87 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "react-native-paper";
+import { StyleSheet, View, Linking } from "react-native";
+import { Button, Text, FAB, IconButton } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import Rating from "../../components/Rating";
+import {
+  Container,
+  InnerContainer,
+  Title,
+  Div,
+  Name,
+  ImageProfile,
+  DivCol,
+  M5,
+  Description,
+  Row,
+  Voltar,
+} from "./styles";
+import MonaLisa from "../../assets/images/mona-lisa.jpg";
 
-const SeeMore = () => {
-  return <Button>Fala</Button>;
+const SeeMore = ({ nome, descricao, avaliacao, km, email }) => {
+  const styles = StyleSheet.create({
+    fab: {
+      position: "absolute",
+      backgroundColor: "#25D366",
+      color: "#ffffff",
+      margin: 16,
+      right: 0,
+      bottom: 0,
+    },
+  });
+
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <IconButton
+            icon="arrow-left"
+            color={"#fff"}
+            size={20}
+            onPress={() => navigation.navigate("MainTab")}
+          ></IconButton>
+          <Voltar>Voltar</Voltar>
+        </Row>
+        <Title>Entre em contato,{"\n"}um maravilhoso serviço te espera!</Title>
+        <InnerContainer>
+          <Div>
+            <ImageProfile source={MonaLisa} />
+            <M5>
+              <Name>Xerox Holmes {nome}</Name>
+              {"\n"}
+              <Rating qtdStar={5}></Rating>
+            </M5>
+          </Div>
+          <View
+            style={{
+              borderBottomColor: "#808080",
+              borderBottomWidth: 0.2,
+            }}
+          />
+          <Div>
+            <Description>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque eget rutrum odio, nec sodales leo. Ut at ultricies
+              urna. Duis aliquet magna dui, quis dapibus tellus imperdiet quis.
+              Nulla porta lacus non mi ullamcorper porta. Praesent nunc felis,
+              dapibus vitae posuere eu, placerat id ligula.{"\n"} {"\n"}Fusce at
+              vestibulum mi. Curabitur sollicitudin quam eu justo elementum,
+              vitae aliquet nulla lacinia. Nam pharetra velit ac quam lobortis,
+              sit amet dignissim purus efficitur.
+            </Description>
+          </Div>
+        </InnerContainer>
+      </Container>
+      <FAB
+        style={styles.fab}
+        icon="whatsapp"
+        color="#fff"
+        onPress={() => console.log("Pressed")}
+      />
+    </>
+  );
 };
 
 export default SeeMore;
---
+++ src/screens/SeeMore/styles.js
@@ -0,0 +1,85 @@
+import { StyleSheet } from "react-native";
+import styled from "styled-components/native";
+import { TextInput, Text } from "react-native-paper";
+
+Container, InnerContainer, Title, Div;
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #1e387d;
+  padding: 55px 10px;
+`;
+
+export const InnerContainer = styled.View`
+  background-color: #fff;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding-bottom: 5%;
+  padding-top: 5%;
+`;
+
+export const Div = styled.View`
+  padding: 0px 25px 0px 25px;
+  margin-bottom: 15px;
+  flex-direction: row;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+`;
+
+export const Title = styled.Text`
+  color: #ffffff;
+  font-size: 25px;
+  margin-top: 80px;
+`;
+
+export const Voltar = styled.Text`
+  color: #ffffff;
+  font-size: 20px;
+  margin-top: 8px;
+`;
+
+export const Description = styled.Text`
+  font-size: 15px;
+  margin-top: 20px;
+  margin-bottom: 45px;
+  color: #363636;
+`;
+
+export const Name = styled.Text`
+  color: #111111;
+  font-size: 20px;
+  margin-bottom: 15px;
+  margin-left: 15px;
+`;
+
+export const M5 = styled.Text`
+  margin-top: 15px;
+  margin-left: 15px;
+`;
+
+export const DivCol = styled.View`
+  flex-direction: column;
+`;
+
+export const ImageProfile = styled.Image`
+  height: 80px;
+  width: 80px;
+  border-radius: 20px;
+`;
+
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 120,
+    borderRadius: 10,
+    backgroundColor: "#1e387d",
+  },
+});
+
+export default styles;
